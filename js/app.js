c5e4aecfccc0ff322cae507b94c3721033411477
Shail Sheth <shethshail56@gmail.com>
2018-09-02 10:33:51 +0530

    Api Connected

--- js/app.js
+++ js/app.js
@@ -2,7 +2,11 @@ var options = {
 	part:'snippet',
 	key:'',
 	q:$('#search-text').val(),
+	maxResults:25,
+
 }
+
+const url = 'https://www.googleapis.com/youtube/v3/search';
 
 $('#search').click(function () {
 	var searchval = $('#search-text').val();
@@ -14,18 +18,54 @@ $('#search').click(function () {
 	
 	if(options.key=='')
 	{
-		var api_key_ = prompt('Please Enter your youtube Api key');
-		if(api_key_==null || api_key_.trim()=='')
+		// var api_key_ = prompt('Please Enter your youtube Api key');
+		// if(api_key_==null || api_key_.trim()=='')
+		// {
+		// 	alert('Your requst has been cancelled');
+		// 	return;
+		// }
+		// else
 		{
-			alert('Your requst has been cancelled');
-			return;
-		}
-		else
-		{
-			options.key=api_key_;
+			options.key='AIzaSyDrIjtZL37bAg9hZz6uIetFCkCHS-xa7Zc' //api_key_;
 		}
 	}
 
+	$.getJSON(url,options,showSearchResult)
 });
+
+function showSearchResult(data)
+{
+	var items = data.items;
+	var snippet='',tubeUrl='',thumbnails='',imgUrl='',imgHeight='',imgWidth='';
+	var title='',channel='',description='';
+	for(var x = 0; x < items.length; x++)
+	{
+		snippet = items[x].snippet;
+		tubeUrl = 'https://www.youtube.com/watch?v=' + items[x].videoId;
+		thumbnails=snippet.thumbnails.high;
+		imgUrl = thumbnails.url;
+		imgHeight = thumbnails.height; 
+		imgWidth = thumbnails.width;
+		title=snippet.title;
+		channel=snippet.channelTitle;
+		description=snippet.description; 
+		
+	$("<div class='row'>"+
+		"<div class='col-12'>"+
+
+		"</div>"+
+	  "</div>"
+	);
+
+
+
+
+
+	}
+
+
+
+	console.log(data);
+}
 
 // AIzaSyDrIjtZL37bAg9hZz6uIetFCkCHS-xa7Zc
